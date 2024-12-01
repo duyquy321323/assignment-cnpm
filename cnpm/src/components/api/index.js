b00b40ac068d17392071f0dfd8fef6ddd07d6f01
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-12-02 02:27:26 +0700

    deploy

--- cnpm/src/components/api/index.js
+++ cnpm/src/components/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'https://print-system-production.up.railway.app/',
+    baseURL: `https://print-system-production.up.railway.app/`,
     withCredentials: true,
 })
 
